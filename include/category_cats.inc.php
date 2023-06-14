485097443f8448367f238d7c3c180564299efb9b
Rodrigo Torres <5768540+darktorres@users.noreply.github.com>
2023-06-14 16:52:20 -0300

    Remove AJAX and lazy load images directly in html

--- include/category_cats.inc.php
+++ include/category_cats.inc.php
@@ -341,7 +341,6 @@ if (count($categories) > 0)
   $derivative_params = trigger_change('get_index_album_derivative_params', ImageStdParams::get_by_type(IMG_THUMB) );
   $tpl_thumbnails_var_selection = trigger_change('loc_end_index_category_thumbnails', $tpl_thumbnails_var_selection);
   $template->assign( array(
-    'maxRequests' =>$conf['max_requests'],
     'category_thumbnails' => $tpl_thumbnails_var_selection,
     'derivative_params' => $derivative_params,
     ) );
