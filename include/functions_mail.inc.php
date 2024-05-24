3d0c8f04e006e51c602b7b29a7324a32525f8c7f
Rodrigo Torres <5768540+darktorres@users.noreply.github.com>
2024-05-24 20:26:08 -0300

    Move PHP dependency phpmailer/phpmailer to Composer

--- include/functions_mail.inc.php
+++ include/functions_mail.inc.php
@@ -607,10 +607,6 @@ function pwg_mail(
         $conf_mail = get_mail_configuration();
     }
 
-    include_once(PHPWG_ROOT_PATH . 'include/phpmailer/Exception.php');
-    include_once(PHPWG_ROOT_PATH . 'include/phpmailer/SMTP.php');
-    include_once(PHPWG_ROOT_PATH . 'include/phpmailer/PHPMailer.php');
-
     $mail = new PHPMailer();
 
     foreach (get_clean_recipients_list($to) as $recipient) {
@@ -939,7 +935,7 @@ function pwg_send_mail_test(
             fwrite($file, 'ERROR: ' . $mail->ErrorInfo . "\n\n");
         }
 
-        fwrite($file, $mail->getSentMIMEMessage());
+        fwrite($file, (string) $mail->getSentMIMEMessage());
         fclose($file);
     }
 }
