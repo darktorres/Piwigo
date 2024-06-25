d7801fa6fe8ba4b1e76a72df3f31d0589f0c536a
Rodrigo Torres <5768540+darktorres@users.noreply.github.com>
2024-06-25 20:18:03 -0300

    Add theme 'Smart Pocket'
    Default theme

---
+++ themes/smartpocket/admin/upgrade.inc.php
@@ -0,0 +1,25 @@
+<?php
+
+if (! defined('PHPWG_ROOT_PATH')) {
+    die('Hacking attempt!');
+}
+
+global $prefixeTable, $conf;
+
+if (! isset($conf['smartpocket'])) {
+    $config = [
+        'loop' => true,
+        //true - false
+        'autohide' => 5000,
+        //5000 - 0
+    ];
+
+    conf_update_param('smartpocket', $config, true);
+} elseif (count(safe_unserialize($conf['smartpocket'])) != 2) {
+    $conff = safe_unserialize($conf['smartpocket']);
+    $config = [
+        'loop' => (empty($conff['loop'])) ? true : $conff['loop'],
+        'autohide' => (empty($conff['autohide'])) ? 5000 : $conff['autohide'],
+    ];
+    conf_update_param('smartpocket', $config, true);
+}
