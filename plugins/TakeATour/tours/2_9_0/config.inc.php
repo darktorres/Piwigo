2b93b8bd406cea46de4d52e7215d249e1f2007f3
Rodrigo Torres <5768540+darktorres@users.noreply.github.com>
2024-06-25 20:12:31 -0300

    Add plugin 'Take A Tour of Your Piwigo'
    Default plugin

---
+++ plugins/TakeATour/tours/2_9_0/config.inc.php
@@ -0,0 +1,54 @@
+<?php
+/**********************************
+ * REQUIRED PATH TO THE TPL FILE */
+
+$TOUR_PATH = PHPWG_PLUGINS_PATH . 'TakeATour/tours/2_9_0/tour.tpl';
+
+/*********************************/
+
+$template->assign('TAT_HAS_ORPHANS', count(get_orphans()) > 0);
+
+// category id for example of delete options. To illustrate the new
+// features, we need an album with photos.
+if (! isset($_SESSION['TAT_tour29_delete_cat_id'])) {
+    $query = '
+SELECT
+    category_id
+  FROM ' . IMAGE_CATEGORY_TABLE . '
+  LIMIT 1
+;';
+    $rows = query2array($query);
+    $_SESSION['TAT_tour29_delete_cat_id'] = count($rows) == 0 ? -1 : $rows[0]['category_id'];
+}
+
+if ($_SESSION['TAT_tour29_delete_cat_id'] > 0) {
+    $template->assign('TAT_tour29_delete_cat_id', $_SESSION['TAT_tour29_delete_cat_id']);
+}
+
+if (! isset($_SESSION['TAT_tour29_image_id'])) {
+    $query = '
+SELECT
+    id
+  FROM ' . IMAGES_TABLE . '
+  ORDER BY id DESC
+  LIMIT 1
+;';
+    $images = query2array($query);
+    $_SESSION['TAT_tour29_image_id'] = count($images) == 0 ? -1 : $images[0]['id'];
+}
+
+if ($_SESSION['TAT_tour29_image_id'] > 0) {
+    $template->assign('TAT_tour29_image_id', $_SESSION['TAT_tour29_image_id']);
+}
+
+$template->assign('TAT_tour29_history_url', 'admin.php?page=stats&year=' . date('Y') . '&month=' . date('n'));
+
+$query = '
+SELECT
+    COUNT(*)
+  FROM ' . TAGS_TABLE . '
+;';
+[$counter] = pwg_db_fetch_row(pwg_query($query));
+if ($counter > 0) {
+    $template->assign('TAT_tour29_has_tags', true);
+}
