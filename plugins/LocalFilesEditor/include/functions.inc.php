5b2db254a1d541f51117c3b39733b1106ec579c4
Rodrigo Torres <5768540+darktorres@users.noreply.github.com>
2024-06-25 20:14:17 -0300

    Add plugin 'LocalFiles Editor'
    Default plugin

---
+++ plugins/LocalFilesEditor/include/functions.inc.php
@@ -0,0 +1,117 @@
+<?php
+
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+/**
+ * returns $code if php syntax is correct
+ * else return false
+ *
+ * @param string php code
+ */
+function eval_syntax(
+    $code
+) {
+    $code = str_replace(['<?php', '?>'], '', $code);
+    if (function_exists('token_get_all')) {
+        $b = 0;
+        foreach (token_get_all($code) as $token) {
+            if ($token == '{') {
+                ++$b;
+            } elseif ($token == '}') {
+                --$b;
+            }
+        }
+        
+        if ($b !== 0) {
+            return false;
+        }
+
+        ob_start();
+        $eval = eval('if(0){' . $code . '}');
+        ob_end_clean();
+        if ($eval === false) {
+            return false;
+        }
+
+    }
+    
+    return '<?php' . $code . '?>';
+}
+
+/**
+ * returns true or false if $str is bool
+ * returns $str if $str is integer
+ * else "$str"
+ *
+ * @param string
+ $value
+ */
+function editarea_quote(
+    $value
+) {
+    return match (gettype($value)) {
+        'boolean' => $value ? 'true' : 'false',
+        'integer' => $value,
+        default => '"' . $value . '"',
+    };
+}
+
+/**
+ * returns bak file for restore
+ * @param string
+ $file
+ */
+function get_bak_file($file)
+{
+    if (get_extension($file) == 'php') {
+        return substr_replace($file, '.bak', strrpos((string) $file, '.'), 0);
+    }
+
+    return $file . '.bak';
+
+}
+
+/**
+ * returns dirs and subdirs
+ * retun array
+ * @param string
+ $path
+ */
+function get_rec_dirs($path = '')
+{
+    $options = [];
+    if (is_dir($path)) {
+        $fh = opendir($path);
+        while ($file = readdir($fh)) {
+            $pathfile = $path . '/' . $file;
+            if ($file !== '.' && $file !== '..' && $file !== '.svn' && is_dir($pathfile)) {
+                $options[$pathfile] = str_replace(['./', '/'], ['', ' / '], $pathfile);
+                $options = array_merge($options, get_rec_dirs($pathfile));
+            }
+        }
+        
+        closedir($fh);
+    }
+    
+    return $options;
+}
---
+++ plugins/LocalFilesEditor/include/tpl.inc.php
@@ -0,0 +1,149 @@
+<?php
+
+if (! defined('PHPWG_ROOT_PATH')) {
+    die('Hacking attempt!');
+}
+
+$edited_file = '';
+
+if (isset($_POST['edit'])) {
+    $_POST['template'] = $_POST['file_to_edit'];
+}
+
+if (! empty($_POST['template'])) {
+    if (preg_match('#\.\./#', (string) $_POST['template'])) {
+        die('Hacking attempt! template extension must be in template-extension directory');
+    }
+
+    if (! preg_match('#\.tpl$#', (string) $_POST['template'])) {
+        die('Hacking attempt! template extension must be a *.tpl file');
+    }
+
+    $template->assign('template', $_POST['template']);
+
+    $edited_file = './template-extension/' . $_POST['template'];
+}
+
+$content_file = '';
+if (file_exists($edited_file)) {
+    $content_file = file_get_contents($edited_file);
+}
+
+$newfile_page = isset($_GET['newfile']);
+
+// Edit new tpl file
+if (isset($_POST['create_tpl'])) {
+    $filename = $_POST['tpl_name'];
+    if (empty($filename)) {
+        $page['errors'][] = l10n('locfiledit_empty_filename');
+    }
+    
+    if (get_extension($filename) != 'tpl') {
+        $filename .= '.tpl';
+    }
+    
+    if (! preg_match('/^[a-zA-Z0-9-_.]+$/', (string) $filename)) {
+        $page['errors'][] = l10n('locfiledit_filename_error');
+    }
+    
+    if (is_numeric($_POST['tpl_model']) && $_POST['tpl_model'] != '0') {
+        $page['errors'][] = l10n('locfiledit_model_error');
+    }
+    
+    if (file_exists($_POST['tpl_parent'] . '/' . $filename)) {
+        $page['errors'][] = l10n('locfiledit_file_already_exists');
+    }
+    
+    if (! empty($page['errors'])) {
+        $newfile_page = true;
+    } else {
+        $template->assign('template', $filename);
+        $edited_file = $_POST['tpl_parent'] . '/' . $filename;
+        $content_file = ($_POST['tpl_model'] == '0') ? '' : file_get_contents($_POST['tpl_model']);
+    }
+}
+
+if ($newfile_page) {
+    $filename = $_POST['tpl_name'] ?? '';
+    $selected['model'] = $_POST['tpl_model'] ?? '0';
+    $selected['parent'] = $_POST['tpl_parent'] ?? PHPWG_ROOT_PATH . 'template-extension';
+
+    // Parent directories list
+    $options['parent'] = [
+        PHPWG_ROOT_PATH . 'template-extension' => 'template-extension',
+    ];
+    $options['parent'] = array_merge($options['parent'], get_rec_dirs(PHPWG_ROOT_PATH . 'template-extension'));
+
+    $options['model'][] = l10n('locfiledit_empty_page');
+    $options['model'][] = '----------------------';
+    $i = 0;
+    foreach (get_extents() as $pwg_template) {
+        $value = PHPWG_ROOT_PATH . 'template-extension/' . $pwg_template;
+        $options['model'][$value] = 'template-extension / ' . str_replace('/', ' / ', $pwg_template);
+        $i++;
+    }
+    
+    foreach (get_dirs($conf['themes_dir']) as $theme_id) {
+        if ($i !== 0) {
+            $options['model'][] = '----------------------';
+            $i = 0;
+        }
+        
+        $dir = $conf['themes_dir'] . '/' . $theme_id . '/template/';
+        if (is_dir($dir) && ($content = opendir($dir))) {
+            while ($node = readdir($content)) {
+                if (is_file($dir . $node) && get_extension($node) == 'tpl') {
+                    $value = $dir . $node;
+                    $options['model'][$value] = $theme_id . ' / ' . $node;
+                    $i++;
+                }
+            }
+        }
+    }
+    
+    if (end($options['model']) == '----------------------') {
+        array_pop($options['model']);
+    }
+    
+    // Assign variables to template
+    $template->assign(
+        'create_tpl',
+        [
+            'NEW_FILE_NAME' => $filename,
+            'MODEL_OPTIONS' => $options['model'],
+            'MODEL_SELECTED' => $selected['model'],
+            'PARENT_OPTIONS' => $options['parent'],
+            'PARENT_SELECTED' => $selected['parent'],
+        ]
+    );
+} else {
+    // List existing template extensions
+    $selected = 0;
+    $options[] = l10n('locfiledit_choose_file');
+    $options[] = '----------------------';
+    foreach (get_extents() as $pwg_template) {
+        $value = $pwg_template;
+        $options[$value] = str_replace('/', ' / ', $pwg_template);
+        if ($edited_file == $value) {
+            $selected = $value;
+        }
+    }
+    
+    if ($selected == 0 && ($edited_file !== '' && $edited_file !== '0')) {
+        $options[$edited_file] = str_replace(['./template-extension/', '/'], ['', ' / '], $edited_file);
+        $selected = $edited_file;
+    }
+    
+    $template->assign(
+        'css_lang_tpl',
+        [
+            'SELECT_NAME' => 'file_to_edit',
+            'OPTIONS' => $options,
+            'SELECTED' => $selected,
+            'NEW_FILE_URL' => $my_base_url . '-tpl&amp;newfile',
+            'NEW_FILE_CLASS' => $edited_file === '' || $edited_file === '0' ? '' : 'top_right',
+        ]
+    );
+}
+
+$codemirror_mode = 'text/html';
