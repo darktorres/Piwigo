a2c08e3aeac4036fb793bf488cdf75a13327ae07
Rodrigo Torres <5768540+darktorres@users.noreply.github.com>
2024-06-26 12:31:13 -0300

    deps(php): add Easy Coding Standard to Composer

---
+++ ecs.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpCsFixer\Fixer\Alias\RandomApiMigrationFixer;
+use PhpCsFixer\Fixer\Basic\SingleLineEmptyBodyFixer;
+use PhpCsFixer\Fixer\Strict\DeclareStrictTypesFixer;
+use PhpCsFixer\Fixer\StringNotation\NoTrailingWhitespaceInStringFixer;
+use Symplify\CodingStandard\Fixer\LineLength\LineLengthFixer;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
+
+return ECSConfig::configure()
+    ->withPaths([
+        __DIR__,
+    ])
+    ->withSkip([
+        __DIR__ . '/_data',
+        __DIR__ . '/galleries',
+        __DIR__ . '/node_modules',
+        __DIR__ . '/tests',
+        __DIR__ . '/themes/bootstrap_darkroom/node_modules',
+        __DIR__ . '/vendor',
+        LineLengthFixer::class,
+    ])
+    ->withRootFiles()
+    ->withPreparedSets(
+        cleanCode: true,
+        common: true,
+        psr12: true,
+        symplify: true
+    )
+    ->withRules([
+        DeclareStrictTypesFixer::class,
+        NoTrailingWhitespaceInStringFixer::class,
+        SingleLineEmptyBodyFixer::class,
+        RandomApiMigrationFixer::class,
+    ])
+;
