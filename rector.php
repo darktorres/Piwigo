a752fe0586c632f77bc79f193a6c474d76964ebb
Rodrigo Torres <5768540+darktorres@users.noreply.github.com>
2024-06-27 08:36:17 -0300

    deps(php): add Rector to Composer

---
+++ rector.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\CodingStyle\Rector\Encapsed\EncapsedStringsToSprintfRector;
+use Rector\Config\RectorConfig;
+use Rector\Php71\Rector\FuncCall\RemoveExtraParametersRector;
+use Rector\Php73\Rector\String_\SensitiveHereNowDocRector;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__,
+    ])
+    ->withSkip([
+        __DIR__ . '/_data',
+        __DIR__ . '/galleries',
+        __DIR__ . '/node_modules',
+        __DIR__ . '/tests',
+        __DIR__ . '/themes/bootstrap_darkroom/node_modules',
+        __DIR__ . '/vendor',
+        EncapsedStringsToSprintfRector::class,
+        RemoveExtraParametersRector::class,
+        SensitiveHereNowDocRector::class,
+    ])
+    ->withRootFiles()
+    ->withPhpSets()
+    ->withPreparedSets(
+        codeQuality: true,
+        codingStyle: true,
+        deadCode: false,
+        earlyReturn: false,
+        instanceOf: false,
+        naming: false,
+        privatization: false,
+        typeDeclarations: true
+    );
