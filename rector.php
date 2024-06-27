1d916aa9c438bd3e03c8cfc1e3dac928c390de69
Rodrigo Torres <5768540+darktorres@users.noreply.github.com>
2024-06-27 08:36:17 -0300

    Add Rector

---
+++ rector.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__,
+    ])
+    ->withSkip([
+        __DIR__ . '/_data',
+        __DIR__ . '/vendor',
+    ])
+    ->withRootFiles()
+    // ->withPhpSets()
+    ->withPreparedSets(
+        codeQuality: false,
+        codingStyle: false,
+        deadCode: false,
+        earlyReturn: false,
+        instanceOf: false,
+        naming: false,
+        privatization: false,
+        typeDeclarations: false
+    );
