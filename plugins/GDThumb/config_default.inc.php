d1a3caa487d22c0c4a432904640d2819271a2f89
Rodrigo Torres <5768540+darktorres@users.noreply.github.com>
2024-07-14 18:25:03 -0300

    feat(plugin): change 'gdThumb' defaults

--- plugins/GDThumb/config_default.inc.php
+++ plugins/GDThumb/config_default.inc.php
@@ -1,16 +1,16 @@
 <?php
 
 $config_default = array(
-  'height'           => 200,
+  'height'           => 600,
   'margin'           => 10,
   'nb_image_page'    => 80,
-  'big_thumb'        => true,
+  'big_thumb'        => false,
   'big_thumb_noinpw' => false,
   'cache_big_thumb'  => true,
   'normalize_title'  => false,
-  'method'           => 'crop',
-  'thumb_mode_album' => 'bottom',
-  'thumb_mode_photo' => 'bottom',
+  'method'           => 'resize',
+  'thumb_mode_album' => 'bottom_static',
+  'thumb_mode_photo' => 'hide',
   'thumb_metamode'   => 'merged',
   'no_wordwrap'      => false
 );
