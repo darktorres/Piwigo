a15a305fc479cb059780bfbb8da96a7d8a006732
Rodrigo Torres <5768540+darktorres@users.noreply.github.com>
2024-07-14 18:18:18 -0300

    config(piwigo): set dark theme as default

--- install/config.sql
+++ install/config.sql
@@ -641,7 +641,7 @@ INSERT INTO
 VALUES
   (
     'modus_theme',
-    'a:5:{s:4:\"skin\";s:9:\"newspaper\";s:16:\"album_thumb_size\";i:250;s:17:\"index_photo_deriv\";s:6:\"2small\";s:22:\"index_photo_deriv_hdpi\";s:6:\"xsmall\";s:19:\"display_page_banner\";b:0;}',
+    'a:5:{s:4:\"skin\";s:6:\"quartz\";s:16:\"album_thumb_size\";i:250;s:17:\"index_photo_deriv\";s:6:\"2small\";s:22:\"index_photo_deriv_hdpi\";s:6:\"xsmall\";s:19:\"display_page_banner\";b:0;}',
     NULL
   );
 
