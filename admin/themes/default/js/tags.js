5da3729103b91b500629ab38d9522851c15e982c
Rodrigo Torres <5768540+darktorres@users.noreply.github.com>
2024-07-18 14:23:37 -0300

    Fix jQuery 1.x deprecations

--- admin/themes/default/js/tags.js
+++ admin/themes/default/js/tags.js
@@ -422,7 +422,7 @@ function duplicateTag(id, name) {
 var selected = [];
 maxItemDisplayed = 5;
 
-$("#toggleSelectionMode").attr("checked", false);
+$("#toggleSelectionMode").prop("checked", false);
 $("#toggleSelectionMode").click(function () {
   selectionMode($(this).is(":checked"));
   $(".tag-info").hide();
