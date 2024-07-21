34fe6c19913fd85e1889d964efc0988fe877e841
Rodrigo Torres <5768540+darktorres@users.noreply.github.com>
2024-07-21 15:35:31 -0300

    Add ESLint

---
+++ eslint.config.mjs
@@ -0,0 +1,29 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+export default [
+    // pluginJs.configs.recommended,
+
+    {
+        ignores: ["_data/*", "galleries/*", "**/node_modules/*", "vendor/*"]
+    },
+
+    {
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                ...globals.jquery
+            }
+        }
+    },
+
+    {
+        plugins: {
+        }
+    },
+    
+    {
+        rules: {
+        },
+    },
+];
