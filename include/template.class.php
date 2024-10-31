bce634c40107661681475359a679b7e9c2817783
Rodrigo Torres <5768540+darktorres@users.noreply.github.com>
2024-10-31 11:14:21 -0300

    style(js): unminify JS/CSS

--- include/template.class.php
+++ include/template.class.php
@@ -1521,7 +1521,7 @@ class ScriptLoader
 
     private static array $known_paths = [
         'core.scripts' => 'themes/default/js/scripts.js',
-        'jquery' => 'node_modules/jquery/dist/jquery.min.js',
+        'jquery' => 'node_modules/jquery/dist/jquery.js',
         'jquery-migrate' => 'node_modules/jquery-migrate/dist/jquery-migrate.js',
         'jquery.ui' => 'https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.10.4/jquery-ui.js',
     ];
