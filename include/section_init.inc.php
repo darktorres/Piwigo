76e3fbb53eca6fbb4d4658a215e781751c9ca2a5
Rodrigo Torres <5768540+darktorres@users.noreply.github.com>
2024-11-14 13:54:50 -0300

    perf(php): ***** cache album items query

--- include/section_init.inc.php
+++ include/section_init.inc.php
@@ -256,22 +256,23 @@ if ($page['section'] == 'categories') {
             $where_sql = "category_id = {$page['category']['id']}";
         }
 
-        if (! isset($cache_key) || ! $persistent_cache->get($cache_key, $page['items'])) {
-            // main query
-            $query = <<<SQL
-                SELECT DISTINCT(image_id)
-                FROM image_category
-                INNER JOIN images ON id = image_id
-                WHERE {$where_sql}
-                    {$forbidden}
-                {$conf['order_by']};
-                SQL;
+        // main query
+        // FIXME: this query fetches all photos of an album everytime rvtscroller asks for few more photos on scroll
+        $query = <<<SQL
+            SELECT DISTINCT(image_id)
+            FROM image_category
+            INNER JOIN images ON id = image_id
+            WHERE {$where_sql}
+                {$forbidden}
+            {$conf['order_by']};
+            SQL;
+        $cache_key = md5($query);
 
+        if (! $persistent_cache->get($cache_key, $page['items'])) {
             $page['items'] = query2array($query, null, 'image_id');
 
-            if (isset($cache_key)) {
-                $persistent_cache->set($cache_key, $page['items']);
-            }
+            // TODO: invalidate this cache on album sync
+            $persistent_cache->set($cache_key, $page['items']);
         }
     }
 } elseif ($page['section'] == 'tags') {
